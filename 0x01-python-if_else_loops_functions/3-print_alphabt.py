774f7bfde038a6b0d44e7e49723cf522b3a66e1d
Santiago Bedoya <santiagobedoa@gmail.com>
2022-05-03 10:49:12 -0500

    Tue May  3 10:49:12 -05 2022

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -2,7 +2,7 @@
 letter = ord('a')
 while letter <= ord('z'):
     if (letter == ord('e') or letter == ord('q')):
-        letter += 1;
+        letter += 1
     else:
         print(chr(letter), end="")
         letter += 1
