0c60546c54e5abda56b728dc89210e9f347db9ca
Santiago Bedoya <santiagobedoa@gmail.com>
2022-05-03 12:56:14 -0500

    Tue May  3 12:56:14 -05 2022

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,8 +2,6 @@
 def uppercase(str):
     for char in str:
         if (ord('a') <= ord(char) and ord(char) <= ord('z')):
-            upper_char = chr(ord(char) - 32)
-            print(upper_char, end="")
-        else:
-            print(char, end="")
+            char = chr(ord(char) - 32)
+        print(char, end="")
     print("\n")
