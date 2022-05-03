6ad8a0662604e283765805c556163b6bf71ffc83
Santiago Bedoya <santiagobedoa@gmail.com>
2022-05-03 10:48:39 -0500

    Tue May  3 10:48:39 -05 2022

---
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+letter = ord('a')
+while letter <= ord('z'):
+    print(chr(letter), end="")
+    letter += 1
---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+letter = ord('a')
+while letter <= ord('z'):
+    if (letter == ord('e') or letter == ord('q')):
+        letter += 1;
+    else:
+        print(chr(letter), end="")
+        letter += 1
