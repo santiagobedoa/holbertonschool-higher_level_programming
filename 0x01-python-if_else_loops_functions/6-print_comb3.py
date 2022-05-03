636be45e56403bf9a1c91a15454ed4d91772365b
Santiago Bedoya <santiagobedoa@gmail.com>
2022-05-03 12:20:05 -0500

    Tue May  3 12:20:05 -05 2022

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for i in range(0, 9):
     for j in range(i+1, 10):
         if (i == 8 and j == 9):
-            print("89\n")
+            print("89")
             break
         print(f"{i}{j}, ", end="")
