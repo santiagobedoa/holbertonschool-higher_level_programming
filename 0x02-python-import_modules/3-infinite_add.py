6d20d67a0eea775664e6c7111f3e80b06f942e81
Santiago Bedoya <santiagobedoa@gmail.com>
2022-05-05 13:38:39 -0500

    Thu May  5 13:38:39 -05 2022

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 from sys import argv
 
+
 def main():
     result = 0
     if (len(argv) == 1):
@@ -9,6 +10,7 @@ def main():
         for i in argv[1:]:
             result += int(i)
     print(result)
+
 
 if __name__ == "__main__":
     main()
