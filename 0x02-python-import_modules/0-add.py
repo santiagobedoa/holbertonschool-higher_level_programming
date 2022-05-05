769d53c224c13488d71d829fa73cb16cd21c16b3
Santiago Bedoya <santiagobedoa@gmail.com>
2022-05-05 12:04:54 -0500

    Thu May  5 12:04:54 -05 2022

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
-import add_0
-
-
-def result():
-    n1, n2 = 1, 2
-    print(f"{n1} + {n2} = {add_0.add(n1, n2)}")
-
-
 if __name__ == "__main__":
-    result()
+    from add_0 import add
+    a = 1
+    b = 2
+    print(f"{a} + {b} = {add(a, b)}")
