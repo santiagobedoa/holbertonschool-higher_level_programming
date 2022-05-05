14a12516645e09bb239cfb2708d7465b73ff8efc
Santiago Bedoya <santiagobedoa@gmail.com>
2022-05-05 12:06:19 -0500

    Thu May  5 12:06:19 -05 2022

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,15 +1,9 @@
 #!/usr/bin/python3
-import calculator_1
-
-
-def result():
+if __name__ == "__main__":
+    import calculator
     a = 10
     b = 5
     print(f"{a} + {b} = {calculator_1.add(a, b)}")
     print(f"{a} - {b} = {calculator_1.sub(a, b)}")
     print(f"{a} * {b} = {calculator_1.mul(a, b)}")
     print(f"{a} / {b} = {calculator_1.div(a, b)}")
-
-
-if __name__ == "__main__":
-    result()
