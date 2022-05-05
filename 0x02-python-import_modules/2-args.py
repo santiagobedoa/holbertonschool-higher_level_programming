50c2fbf7a4c2257b82ad33051ba75e8bf025bf38
Santiago Bedoya <santiagobedoa@gmail.com>
2022-05-05 13:21:32 -0500

    Thu May  5 13:21:32 -05 2022

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
         print("1 argument:")
         print(f"1: {argv[1]}")
     else:
-        print(f"{len(argv)} arguments:")
+        print(f"{len(argv) - 1} arguments:")
         for i, j in enumerate(argv):
             if (i != 0):
                 print(f"{i}: {j}")
