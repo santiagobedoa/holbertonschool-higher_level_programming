b702e0a29d2435995f0c55e4bd901fdb3560a06b
Santiago Bedoya <santiagobedoa@gmail.com>
2022-05-09 15:24:41 -0500

    Mon May  9 15:24:41 -05 2022

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -2,11 +2,12 @@
 
 
 def delete_at(my_list=[], idx=0):
-    if idx < 0 or idx > len(my_list) -1:
+    if idx < 0 or idx > len(my_list) - 1:
         return my_list
     else:
         new_list = []
         for i, j in enumerate(my_list):
             if i != idx:
                 new_list.append(j)
+        my_list = new_list
         return new_list
