f984e28ec5774a1e285e7616d487484bd244aa30
Santiago Bedoya <santiagobedoa@gmail.com>
2022-05-09 13:42:50 -0500

    Mon May  9 13:42:50 -05 2022

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,5 +2,6 @@
 
 
 def print_reversed_list_integer(my_list=[]):
-    for i in range(0, len(my_list)):
-        print(my_list[len(my_list) - i - 1])
+    if my_list:
+        for i in range(0, len(my_list)):
+            print(my_list[len(my_list) - i - 1])
