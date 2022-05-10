20844cd9ee0b1dbdf195f9027aed9c65eac90f43
Santiago Bedoya <santiagobedoa@gmail.com>
2022-05-10 18:48:04 -0500

    Tue May 10 18:48:04 -05 2022

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+
+def complex_delete(a_dictionary, value):
+    keys = list(a_dictionary.keys())
+    values = list(a_dictionary.values())
+    if value in values:
+        del a_dictionary[keys[values.index(value)]]
+    return a_dictionary
