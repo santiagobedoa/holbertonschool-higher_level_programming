d041d6836ac2ee346406195fc573d0514778c34f
Santiago Bedoya <santiagobedoa@gmail.com>
2022-05-10 16:46:06 -0500

    Tue May 10 16:46:06 -05 2022

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,6 +3,6 @@
 
 def best_score(a_dictionary):
     if a_dictionary:
-        higher_score = max(list(a_dictionary.values()))
-        return (list(a_dictionary.keys())[list(a_dictionary.values()).index(higher_score)])
+        h = max(list(a_dictionary.values()))
+        return list(a_dictionary.keys())[list(a_dictionary.values()).index(h)]
     return None
