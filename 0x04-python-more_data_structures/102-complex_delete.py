1044a3be07df645a26ecc1b380127de8395bfafc
Santiago Bedoya <santiagobedoa@gmail.com>
2022-05-10 20:22:53 -0500

    Tue May 10 20:22:53 -05 2022

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -2,10 +2,8 @@
 
 
 def complex_delete(a_dictionary, value):
-    keys = list(a_dictionary.keys())
-    values = list(a_dictionary.values())
-    if value in values:
-        while value in values:
-            del a_dictionary[keys[values.index(value)]]
-            values.pop(values.index(value))
+    copy = a_dictionary.copy()
+    for k, v in copy.items():
+        if v == value:
+            del a_dictionary[k]
     return a_dictionary
