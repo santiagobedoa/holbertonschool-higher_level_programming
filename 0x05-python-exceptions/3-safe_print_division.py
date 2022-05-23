50051e03eb3ce3fd5f8a48da34b39500bff47736
Santiago Bedoya <santiagobedoa@gmail.com>
2022-05-23 14:22:56 -0500

    Mon May 23 14:22:56 -05 2022

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,12 +2,11 @@
 
 
 def safe_print_division(a, b):
-    result = 0
     try:
         result = a / b
-        print(f"Inside result: {result}")
+        print("Inside result: {:.1f}".format(result))
     except ZeroDivisionError:
         result = None
-        print(f"Inside result: {result}")
+        print("Inside result: {:.1f}".format(result))
     finally:
         return result
