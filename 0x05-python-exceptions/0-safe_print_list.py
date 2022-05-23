0126466c59da76a5751c17616390774fa1ff6a47
Santiago Bedoya <santiagobedoa@gmail.com>
2022-05-23 12:44:59 -0500

    Mon May 23 12:44:59 -05 2022

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,10 +3,11 @@
 
 def safe_print_list(my_list=[], x=0):
     len_list = 0
-    for element in my_list:
-        print(element, end="")
-        len_list += 1
-        if len_list == x:
-            break
-    print("\n")
+    for i in range(0, x):
+        try:
+            print(my_list[i], end="")
+            len_list += 1
+        except IndexError:
+            pass
+    print("")
     return len_list
