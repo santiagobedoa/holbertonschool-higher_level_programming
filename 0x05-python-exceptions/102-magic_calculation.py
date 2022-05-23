cd157d58100c02e998af3dd37a5bec8b05d66550
Santiago Bedoya <santiagobedoa@gmail.com>
2022-05-23 15:50:44 -0500

    Mon May 23 15:50:44 -05 2022

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -8,7 +8,7 @@ def magic_calculation(a, b):
             if i > a:
                 raise Exception('Too far')
             else:
-                result = (a ** b) / i
+                result += (a ** b) / i
         except Exception as e:
             result = b + a
             break
