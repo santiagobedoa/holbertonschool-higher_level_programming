b77af28e032e9d66bbbe9627a2d3633a0c5e891c
Santiago Bedoya <santiagobedoa@gmail.com>
2022-05-23 14:23:51 -0500

    Mon May 23 14:23:51 -05 2022

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -7,6 +7,6 @@ def safe_print_division(a, b):
         print("Inside result: {:.1f}".format(result))
     except ZeroDivisionError:
         result = None
-        print("Inside result: {:.1f}".format(result))
+        print("Inside result: {}".format(result))
     finally:
         return result
