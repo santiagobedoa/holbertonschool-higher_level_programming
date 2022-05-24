a42381961f7b509a30aba5bbb88d76e901d16923
Santiago Bedoya <santiagobedoa@gmail.com>
2022-05-24 16:04:45 -0500

    Tue May 24 16:04:45 -05 2022

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Square Class"""
+
+
+class Square:
+    """Square Class without methods or public attributes"""
+    
+    def __init__(self, size):
+        self.__size = size
