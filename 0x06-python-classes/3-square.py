21790ccefefde628a4cdf8fe74a6dcdd41fbc87a
Santiago Bedoya <santiagobedoa@gmail.com>
2022-05-24 16:24:53 -0500

    Tue May 24 16:24:53 -05 2022

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""defines a Square Class"""
+
+
+class Square:
+    """Represents a Square"""
+
+    def __init__(self, size=0):
+        """initializes"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """calculates the square's area"""
+        return (self.__size) ** 2
