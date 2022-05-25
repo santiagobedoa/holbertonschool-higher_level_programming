c2b576b8af3788f3b9961b5fefeb0dc4742c510c
Santiago Bedoya <santiagobedoa@gmail.com>
2022-05-24 20:23:06 -0500

    Tue May 24 20:23:06 -05 2022

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Class MagicClass"""
+import math
+
+
+class MagicClass:
+    def __init__(self, radius=0):
+        """initialize MagicClass"""
+        self.__radius = 0
+        if type(radius) is not int and type(radius) is not float:
+                raise TypeError('radius must be a number')
+        self.__radius = radius
+
+    def area(self):
+        """area MagicClass"""
+        return self.__radius ** 2 * math.pi
+
+    def circumference(self):
+        """circumference MagicClass"""
+        return 2 * math.pi * self.__radius
