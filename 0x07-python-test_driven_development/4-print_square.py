35ff1237c6ab400e76008f89dbeafd118bbf705f
Santiago Bedoya <santiagobedoa@gmail.com>
2022-05-27 17:28:47 -0500

    Fri May 27 17:28:47 -05 2022

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+This is the "4-print_square.py" module.
+
+The 4-print_square.py module supplies one function, print_square(size).
+"""
+
+
+def print_square(size):
+    """
+        Prints a square with the character "#"
+    """
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    print((("#" * size + "\n") * size), end="")
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/4-print_square.txt")
