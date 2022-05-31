d1b283279c5c80d8b70bf55e42b628ac107ecd47
Santiago Bedoya <santiagobedoa@gmail.com>
2022-05-31 18:13:08 -0500

    Tue May 31 18:13:08 -05 2022

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -54,12 +54,12 @@ class Rectangle:
 
     def perimeter(self):
         """ Returns the area of the Rectangle """
-        if not self.__width or not self.__height:
+        if self.__width == 0 or self.__height == 0:
             return 0
         return (self.__width + self.__height) * 2
 
     def __str__(self):
         """ Returns string representation with '#' """
-        if not self.__width or not self.__height:
+        if self.__width == 0 or self.__height == 0:
             return " "
         return (("#" * self.__width + '\n') * self.height)[:-1]
