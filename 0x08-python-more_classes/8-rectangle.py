3b9aa6ea68b02debad3ff414344ef36689d9f824
Santiago Bedoya <santiagobedoa@gmail.com>
2022-05-31 19:04:17 -0500

    Tue May 31 19:04:17 -05 2022

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -93,6 +93,6 @@ class Rectangle:
             raise TypeError("rect_1 must be an instance of Rectangle")
         if type(rect_2) is not Rectangle:
             raise TypeError("rect_2 must be an instance of Rectangle")
-        if rect_1.area() > rect_2.area():
-            return rect_1
-        return rect_2
+        if rect_1.area() < rect_2.area():
+            return rect_2
+        return rect_1
