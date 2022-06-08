87807b15e99b6b22ae1822a88739ea74449bd669
Santiago Bedoya <santiagobedoa@gmail.com>
2022-06-08 16:55:23 -0500

    Wed Jun  8 16:55:23 -05 2022

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -3,5 +3,6 @@
 
 
 def append_write(filename="", text=""):
+    ''' appends a string at the end of a text file '''
     with open(filename, "a", encoding="utf-8") as f:
         return f.write(text)
