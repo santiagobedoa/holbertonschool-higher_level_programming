8e9136c316a6ce50df6c3dd10c6ef1e877b764c2
Santiago Bedoya <santiagobedoa@gmail.com>
2022-07-12 18:53:09 -0500

    Tue Jul 12 18:53:09 -05 2022

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,4 @@
+-- Lists all records of second_table
+-- Results should display both the score and the name
+-- Records should be ordered by score (top first)
+SELECT score, name FROM second_table ORDER BY score DESC;
