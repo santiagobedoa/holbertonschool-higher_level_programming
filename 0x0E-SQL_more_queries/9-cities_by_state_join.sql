49d28bafc5a70b4a4278c83cdcd05c43caeea56b
Santiago Bedoya <santiagobedoa@gmail.com>
2022-07-14 16:17:43 -0500

    Thu Jul 14 16:17:43 -05 2022

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,4 @@
+-- lists all cities contained in the database hbtn_0d_usa
+-- Each record should display: cities.id - cities.name - states.name
+-- You can use only one SELECT statement
+SELECT cities.id, cities.name, states.name FROM cities LEFT JOIN states ON cities.state_id = states.id ORDER BY states.id;
