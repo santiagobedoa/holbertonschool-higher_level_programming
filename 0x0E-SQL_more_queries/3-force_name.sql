da1aefbe86059096508f6626f48fd8b8655bfe1e
Santiago Bedoya <santiagobedoa@gmail.com>
2022-07-14 16:02:51 -0500

    Thu Jul 14 16:02:51 -05 2022

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,5 +1,3 @@
 -- creates the table force_name
-CREATE TABLE IF NOT EXISTS force_name (
-    id INT,
-    name VARCHAR(256) NOT NULL;
-) ENGINE = INNODB;
+CREATE TABLE IF NOT EXISTS force_name (id INT,
+name VARCHAR(256) NOT NULL);
