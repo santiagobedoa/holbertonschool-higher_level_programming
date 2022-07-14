e384a9a49bde74788d271337f3ea7e1bd9a8cd75
Santiago Bedoya <santiagobedoa@gmail.com>
2022-07-14 18:52:10 -0500

    Thu Jul 14 18:52:10 -05 2022

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,5 @@
+-- lists all Comedy shows in the database hbtn_0d_tvshows
+SELECT tv_shows.title
+FROM tv_genres, tv_show_genres, tv_shows
+WHERE tv_genres.name = "Comedy" AND tv_show_genres.genre_id = tv_genres.id AND tv_show_genres.show_id = tv_shows.id
+ORDER BY tv_shows.title ASC;
