7cd80e5216cd5e81dd42411b370c60aaf661bb35
Santiago Bedoya <santiagobedoa@gmail.com>
2022-08-15 18:03:43 -0500

    Mon Aug 15 18:03:43 -05 2022

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,7 +2,7 @@
 let r = 0;
 const numbers = process.argv.slice(2);
 if (numbers.length > 1) {
-  numbers.sort();
+  numbers.sort((a, b) => a - b);
   r = numbers[numbers.length - 2];
 }
 console.log(r);
