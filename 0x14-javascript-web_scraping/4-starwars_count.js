d691d8b66d0ad094b58feacb229fad1d0db4fff5
Santiago Bedoya <santiagobedoa@gmail.com>
2022-08-17 12:33:15 -0500

    v1

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+request (url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    const films = JSON.parse(body).results;
+    let counter = 0;
+    for (const film in films) {
+      const characters = films[film].characters;
+      for (const char in characters) {
+        if (characters[char].includes('18')) {
+          counter++;
+        }
+      }
+    }
+    console.log(counter);
+  }
+});
