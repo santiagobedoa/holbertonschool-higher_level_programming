d51331eb9760bbb49ae19fd153a7c620edff18aa
Santiago Bedoya <santiagobedoa@gmail.com>
2022-08-17 12:08:10 -0500

    Wed Aug 17 12:08:10 -05 2022

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/:id';
+const episode = process.argv[2];
+request(url + episode, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    const jsonObj = JSON.parse(body);
+    console.log(jsonObj.title);
+  }
+});
