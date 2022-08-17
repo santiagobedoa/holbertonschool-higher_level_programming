352d7e98fec3108562c425454f0b01bdbefd08af
Santiago Bedoya <santiagobedoa@gmail.com>
2022-08-17 12:35:23 -0500

    bugs fixed

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const request = require('request');
 const url = process.argv[2];
-request (url, function (err, response, body) {
+request(url, function (err, response, body) {
   if (err) {
     console.log(err);
   } else if (response.statusCode === 200) {
