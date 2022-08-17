ca40bc52030db2712b9fc644e5088b9010a7f006
Santiago Bedoya <santiagobedoa@gmail.com>
2022-08-17 12:15:29 -0500

    bugs fixed

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
-const url = 'https://swapi-api.hbtn.io/api/films/:id';
+const url = 'https://swapi-api.hbtn.io/api/films/';
 const episode = process.argv[2];
 request(url + episode, function (err, response, body) {
   if (err) {
