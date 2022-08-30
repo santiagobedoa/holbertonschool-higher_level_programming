2af7a93169f559cea9a03ad6ad930880f009425f
Santiago Bedoya <santiagobedoa@gmail.com>
2022-08-30 14:58:21 -0500

    Tue Aug 30 14:58:21 -05 2022

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,18 +1,44 @@
 #!/usr/bin/python3
 """List all states that matches the arg"""
 
-import MySQLdb
-from sys import argv
-
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                         passwd=argv[2], db=argv[3], charset="utf8")
-    cur = db.cursos()
-    cur.execute("SELECT * FROM states WHERE states.name like {:s} \
-                ORDER BY states.id ASC".format(argv[4]))
-    rows = cur.fetchall()
-    for row in rows:
-        if row[1] == argv[4]:
-            print(row)
-    cur.close()
-    db.close()
+    import MySQLdb
+    from sys import argv
+
+    if (len(argv) == 5):
+        user = argv[1]
+        password = argv[2]
+        database = argv[3]
+        argument = argv[4]
+        config = {
+            'user': user,
+            'passwd': password,
+            'host': 'localhost',
+            'db': database,
+            'port': 3306,
+        }
+        db = MySQLdb.connect(**config)
+
+        with db.cursor() as cursor:
+            """DON'T DO QUERY IN THIS FORM"""
+            query = """
+                SELECT * FROM states WHERE name
+                LIKE '{:s}' ORDER BY id ASC""".format(argument)
+
+            cursor.execute(query)
+
+            data = cursor.fetchall()
+
+        if not data:
+            """
+            print("Doesn't exist {0} state".format(argument))
+            """
+        else:
+            [print(state) for state in data]
+
+        db.close()
+
+    else:
+        """
+        print("Usage: ./0-select_states.py username password database state")
+        """
