5096c076a3025c168db4092850591b2744b0eff0
Santiago Bedoya <santiagobedoa@gmail.com>
2022-08-30 11:06:46 -0500

    Tue Aug 30 11:06:46 -05 2022

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""list all objects from a database"""
+
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+from sqlalchemy import create_engine
+from model_state import Base, State
+
+if __name__ == "__name__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}\
+                            ".format(argv[1], argv[2], argv[3]))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    Base.metadata.create_all(engine)
+    for state in session.query(State).order_by(State.id):
+        print("{}: {}".format(state.id, state.name))
+    session.close()
