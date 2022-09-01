b29556c5bc71186141ab5ff673059b6e7a18ef68
Santiago Bedoya <santiagobedoa@gmail.com>
2022-09-01 10:44:16 -0500

    Thu Sep  1 10:44:16 -05 2022

---
+++ 0x10-python-network_0/1-body.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+# takes in a URL, sends a GET request to the URL
+# and displays the body of the response
+curl -Ls "$1"
---
+++ 0x10-python-network_0/2-delete.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+# sends a DELETE request
+curl -sX DELETE "$1"
