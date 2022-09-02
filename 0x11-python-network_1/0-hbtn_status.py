627956b441bf6891ee14d8bb6daa9dc288e1924e
Santiago Bedoya <santiagobedoa@gmail.com>
2022-09-02 15:08:33 -0500

    Fri Sep  2 15:08:33 -05 2022

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Python script that fetches https://intranet.hbtn.io/status
+"""
+
+from cgitb import html
+from urllib import request
+
+if __name__ == "__main__":
+    with request.urlopen("https://intranet.hbtn.io/status") as response:
+        html = response.read()
+        print("Body response:")
+        print("\t- type:", type(html))
+        print("\t- content:", html)
+        print("\t- utf8 content:", html.decode('utf8'))
