48091f165882bbde1d071034348f7712c56f05d8
Santiago Bedoya <santiagobedoa@gmail.com>
2022-09-02 15:10:13 -0500

    Fri Sep  2 15:10:13 -05 2022

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,10 +6,9 @@ Python script that fetches https://intranet.hbtn.io/status
 from cgitb import html
 from urllib import request
 
-if __name__ == "__main__":
-    with request.urlopen("https://intranet.hbtn.io/status") as response:
-        html = response.read()
-        print("Body response:")
-        print("\t- type:", type(html))
-        print("\t- content:", html)
-        print("\t- utf8 content:", html.decode('utf8'))
+with request.urlopen("https://intranet.hbtn.io/status") as response:
+    html = response.read()
+    print("Body response:")
+    print("\t- type:", type(html))
+    print("\t- content:", html)
+    print("\t- utf8 content:", html.decode('utf8'))
