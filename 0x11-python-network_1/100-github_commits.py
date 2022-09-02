23a6d44dc4528acb3a8c79998bb66ba8909b796e
Santiago Bedoya <santiagobedoa@gmail.com>
2022-09-02 16:58:50 -0500

    Fri Sep  2 16:58:50 -05 2022

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -5,14 +5,13 @@ Python script that takes 2 arguments in order to solve this challenge
 """
 
 from sys import argv
-from urllib import response
 import requests
 
 if __name__ == "__main__":
     repo = argv[1]
     username = argv[2]
     url = "https://api.github.com/repos/{}/{}/commits".format(username, repo)
-    response = response.get(url)
+    response = requests.get(url)
     for commit in response.json()[:10]:
         print(
             "{}: {}".format(
