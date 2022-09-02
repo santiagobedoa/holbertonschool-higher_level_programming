81e43a31bfeab826fd9f2bb57266c8a596c202c7
Santiago Bedoya <santiagobedoa@gmail.com>
2022-09-02 15:37:32 -0500

    Fri Sep  2 15:37:32 -05 2022

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Python script that fetches https://intranet.hbtn.io/status
+"""
+
+import requests
+
+if __name__ == "__main__":
+    response = requests.get("https://intranet.hbtn.io/status")
+    print("Body response:\n\t- type: {}\n\t- content: {}"
+          .format(type(response.text), response.text))
