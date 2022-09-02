aa1838883e590f8280da0e56f1da339e1adfd049
Santiago Bedoya <santiagobedoa@gmail.com>
2022-09-02 15:27:28 -0500

    Fri Sep  2 15:27:28 -05 2022

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a URL and an email, sends a POST request 
+to the passed URL with the email as a parameter, 
+and displays the body of the response (decoded in utf-8)
+"""
+
+from urllib import request, parse, response
+import sys
+
+if __name__ == "__main__":
+    data = parse.urlencode({"email": sys.argv[2]}).encode()
+    with request.urlopen(sys.argv[1], data) as response:
+        print(response.read().decode('utf-8'))
