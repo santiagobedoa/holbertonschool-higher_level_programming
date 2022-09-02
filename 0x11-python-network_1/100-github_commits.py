3c1001a503f25ca278a71acd32e95b923accd342
Santiago Bedoya <santiagobedoa@gmail.com>
2022-09-02 16:52:33 -0500

    Fri Sep  2 16:52:33 -05 2022

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Holberton challenge
+"""
+
+from sys import argv
+from urllib import response
+import requests
+
+if __name__ == "__main__":
+    repo = argv[1]
+    username = argv[2]
+    url = "https://api.github.com/repos/{}/{}/commits".format(username, repo)
+    response = response.get(url)
+    for commit in response.json()[:10]:
+        print(
+            "{}: {}".format(
+                commit['sha'], commit['commit']['author']['name']
+            )
+        )
